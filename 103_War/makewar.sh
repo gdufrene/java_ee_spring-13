faaada2ffb9a5b7393a6f5748c4b8eea56b6203c
gdufrene <guillaume.dufrene@free.fr>
2013-10-20 22:18:01 +0200

    Update script to comply with web-inf/classes

--- 103_War/makewar.sh
+++ 103_War/makewar.sh
@@ -4,14 +4,9 @@ JETTY_VERSION="9.0.4.v20130625"
 JETTY_DIR=/Volumes/HD/Install/jetty-distribution-${JETTY_VERSION}
 
 CP="bin"
-if [ ! -d bin/fr ] ; then
+if [ ! -d WEB-INF/classes/fr ] ; then
   echo "classes does not exists, compile first."
   exit 1
 fi
-test -d WEB-INF/classes && rm -rf WEB-INF/classes 
-mkdir WEB-INF/classes
-cp -r bin/fr WEB-INF/classes
 jar cvf todo.war www WEB-INF
 
-test -d "bin" || mkdir "bin"
-javac -classpath "$CP" -sourcepath "src" -d "bin" "src/fr/eservice/todo/TodoList.java"
