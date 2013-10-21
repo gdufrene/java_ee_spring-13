f1a84d6be8759b82d4b59c9630a780ef29f2109d
gdufrene <guillaume.dufrene@free.fr>
2013-10-21 23:40:58 +0200

    Revert "Implement servlet and jsp view"
    This reverts commit e9acd3ed5250940b19c79340a1e8dbe83d706232.
    
    Conflicts:
    
    	103_War/src/fr/eservice/todo/Task.java

--- 103_War/src/fr/eservice/todo/TodoServlet.java
+++ 103_War/src/fr/eservice/todo/TodoServlet.java
@@ -1,16 +1,12 @@
 package fr.eservice.todo;
 
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletResponse;
 
 import fr.eservice.todo.TodoList.ORDER;
 
@@ -18,13 +14,11 @@ public class TodoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	private TodoList todoList;
-	private DateFormat df;
 	
 	@Override
 	public void init() throws ServletException {
 		super.init();
 		todoList = new TodoList();
-		df = new SimpleDateFormat("dd/MM/yyyy");
 	}
 	
 	enum ACTION {
@@ -36,83 +30,29 @@ public class TodoServlet extends HttpServlet {
 
 	@Override
 	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// TODO Complete this code ...
 		String _act = req.getParameter("action");
 		if ( _act == null ) _act = "listtask";
 		ACTION act = ACTION.valueOf( _act.toUpperCase() );
 		switch( act ) {
 		case ADDTASK:
-			String title = req.getParameter("title");
-			String description = req.getParameter("description");
-			
-			Long target = null;
-			String input_date = req.getParameter("target");
-			if ( input_date == null ) input_date = "";
-			input_date.trim();
-			try {
-				if ( !input_date.isEmpty() ) {
-					target = df.parse( input_date ).getTime() / 1000L;
-				}
-				Task task = Task.create(title, description, target);
-				todoList.addTask(task);
-			} catch (ParseException e){
-				showError( res, "La date que vous avez saisi n'est pas correcte." );
-				return;
-			} catch (ParameterException pe) {
-				showError(res, "Erreur de saisi d'un paramètre.<br/>"+pe.getMessage());
-				return;
-			}
-			((HttpServletResponse)res).sendRedirect("/index.html");
+			// TODO
 			break;
 		case DELTASK:
-			int taskRef = getValidTaskRef( res, req.getParameter("tid") );
-			if ( taskRef < 0 ) return;
-			todoList.removeTask(taskRef);
-			((HttpServletResponse)res).sendRedirect("/index.html");
+			// TODO
 			break;
 		case COMPLETETASK:
-			taskRef = getValidTaskRef( res, req.getParameter("tid") );
-			todoList.completeTask(taskRef);
-			((HttpServletResponse)res).sendRedirect("/index.html");
+			// TODO
 			break;
 		case LISTTASK:
+			// TODO Complete this code if needed ...
 			req.setAttribute("tasks", todoList.getTasks(ORDER.TARGET_DATE));
-			req.setAttribute("dateFormatter", df);
 			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/www/list_tasks.jsp");
 	        dispatcher.forward(req, res);
 	        return;
 		default:
 			res.getWriter().print("You must specify a valid action.");
 			return;
-		}
-	}
-
-	private int getValidTaskRef(ServletResponse res, String reference) {
-		if ( reference == null ) reference = "";
-		reference.trim();
-		if ( reference.isEmpty() ) {
-			showError(res, "Vous devez indiquer le numéro de la tache à effacer.");
-			return -1;
-		}
-		int taskRef = 0;
-		try {
-			taskRef = Integer.parseInt(reference);				
-		} catch (NumberFormatException e) {
-			showError(res, "Le numéro de tâche doit être un entier valide");
-			return -1;
-		}
-		return taskRef;
-	}
-
-	private void showError(ServletResponse res, String errorMessage) {
-		res.setContentType("text/html");
-		try {
-			res.getWriter().print(
-				"<html><head><title>Error</title></head><body>" +
-				"<h1>Error</h1><b>" + errorMessage + "</b>" +
-				"</body></html>"
-			);
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
 	}
 	
--- 103_War/www/list_tasks.jsp
+++ 103_War/www/list_tasks.jsp
@@ -1,11 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"
-    import="
-fr.eservice.todo.Task,
-java.text.DateFormat,
-java.text.SimpleDateFormat,
-java.util.Date
-"
+    import="fr.eservice.todo.Task"
 %>
 <!DOCTYPE html>
 <html>
@@ -13,104 +8,53 @@ java.util.Date
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Ma liste de choses à faire</title>
 </head>
-<style>
-li {
-  border-left: solid 2px #CCC;
-  list-style-type: none;
-  padding-left: 10px;
-  background-color: #EEE;
-  max-width: 500px;
-  margin-bottom: 10px;
-}
-.taskList {
-  position: absolute;
-  left: 300px;
-  width: 50%;
-}
-.deltask {
-  float: left;
-}
-.completetask {
-  float: right;
-}
-li.complete {
-  background-color: #EFE;
-}
-.addTask {
-  position: absolute;
-  width: 250px;
-  overflow: hidden;
-}
-</style>
 <body>
 <h1>Todo liste</h1>
 <%
 Task[] tasks = (Task[]) request.getAttribute("tasks");
 
 if ( tasks == null || tasks.length == 0 ) { %>
-<i>Aucune tâche n'est enregistrée</i>.<br/>
+<i>Aucune tâche n'est enregistré</i>.<br/>
 <% } else { %>
 
 <%-- 
   /*--------------- Liste des tâches -----------------*/
 --%>
-<div class="taskList">
-<b>Liste des tâches</b>
-<ul>
-<% 
-DateFormat df = (DateFormat) request.getAttribute("dateFormatter");
-if ( df == null ) df = new SimpleDateFormat();
-for( Task t : tasks ) { 
-%>
-  <li class="<%= (t.hasBeenCompleted() ? "complete" : "todo") %>">
-    <b><%= t.getTitle() %></b><br/>
-    <p><%= t.getDescription() %></p>
-    <% if (t.getTargetDate() != 0L) { %>
-      A terminer avant le : <%= df.format( new Date(t.getTargetDate()*1000L) ) %>
-    <% } %><br/>
-    <div class="deltask">
-      [ <a href="?action=DELTASK&tid=<%= t.getReference() %>"> Effacer </a> ]
-    </div>
-    <% if ( !t.hasBeenCompleted() ) { %>
-    <div class="completetask">
-      [ <a href="?action=COMPLETETASK&tid=<%= t.getReference() %>"> Noter comme terminée </a> ] 
-    </div>
-    <% } %>
-    <div style="clear: both"></div>
-  </li>
-<% 
-} // -- /for tasks/ --
-%>
-</ul>
-</div>
+<b>TODO: Lister les tâches</b>
+
+<pre>
+  "Titre de la tache" à réaliser avant le "xx/xx/xxxx"
+  "Description de la tache"
+  ...
+  <a href="xxx">Cliquer ici</a> pour noter cette tâche terminée.
+  <a href="xxx">Cliquer ici</a> pour supprimer cette tâche terminée.
+</pre>
 
 <% } %>
 
 <%-- 
   /*--------------- Formulaire d'ajout d'une tâche -----------------*/
 --%>
-<div class="addTask">
-<form name="TaskForm" method="POST">
-  <input type="hidden" name="action" value="addtask">
+<form name="" method="">
+  <input type="hidden" name="action" value="">
   
   <p>
   Titre :<br/>
-  <input type="text" name="title" value=""><br/>
+  <input type="" name="" value=""><br/>
   </p>
   
   <p>
   Description :<br/>
-  <textarea name="description"></textarea>
+  <textarea name=""></textarea>
   </p>
   
   <p>
   Date Limite de réalisation :<br/>
-  <input type="text" name="target" value=""></input>
+  <input type="" name="" value=""></input>
   </p>
   
   <input type="submit" value="Créer la tâche"/>
 </form>
-</div>
 
 </body>
 </html>
