ceffe76a4e6104d5743ddaffb70377ce08437621
gdufrene <guillaume.dufrene@free.fr>
2013-11-02 17:25:56 +0100

    Replace implementation with TODOs

--- 105_SpringBasics/src/fr/eservice/ElevatorSoftware.java
+++ 105_SpringBasics/src/fr/eservice/ElevatorSoftware.java
@@ -1,24 +1,16 @@
 package fr.eservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import fr.eservice.common.DisplayPanel;
-import fr.eservice.common.DisplayPanel.Going;
 import fr.eservice.common.DisplayPanel.Level;
 import fr.eservice.common.Elevator;
 import fr.eservice.common.ElevatorListener;
 
-@Component
+
 public class ElevatorSoftware implements ElevatorListener, ElevatorUserInterface {
 	
-	@Autowired
 	private Elevator elevator;
 	
-	@Autowired
 	private DisplayPanel panel;
-	
-	private int targetFloor = -1;
 	
 	public void init() {
 		elevator.start();
@@ -29,40 +21,18 @@ public class ElevatorSoftware implements ElevatorListener, ElevatorUserInterface
 	@Override
 	public void callAt(int level) {
 		System.out.println("Software:callAt " + level);
-		if ( level < 1 || level > 5 ) return;
-		if ( targetFloor > 0 ) return;
-		
-		int currentLevel = elevator.currentFloorLevel();
-		targetFloor = level;
-		if ( level == currentLevel ) {
-			elevator.openDoors();
-			return;
-		}
-		
-		
-		if ( targetFloor > currentLevel ) panel.setGoing(Going.UP);
-		else panel.setGoing(Going.DOWN);
-		
-		elevator.closeDoors();
+		// TODO 
 	}
 	
 	@Override
 	public void wantGo(int level) {
-		callAt( level );
+		// TODO 
 	}
 	
 	@Override
 	public void doorOpened() {
 		System.out.println("Software:doorOpened");
-		mayKeepOpen = true;
-		int level = elevator.currentFloorLevel();
-		System.out.println("Welcome to floor " + elevator.currentFloorLevel() );
-		if ( level != targetFloor ) {
-			elevator.closeDoors();
-		} else {
-			targetFloor = -1;
-			panel.setGoing(Going.NONE);
-		}
+		// TODO
 	}
 	
 	boolean mayKeepOpen = false;
@@ -70,21 +40,13 @@ public class ElevatorSoftware implements ElevatorListener, ElevatorUserInterface
 	@Override
 	public void doorClosed() {
 		System.out.println("Software:doorClosed");
-		if ( targetFloor > 0 ) {
-			mayKeepOpen = false;
-			atFloor( elevator.currentFloorLevel() );
-		}
+		// TODO
 	}
 
 	@Override
 	public void atFloor(int floor) {
 		System.out.println("At floor ... " + floor);
-		if ( targetFloor < 0 || targetFloor == floor ) {
-			elevator.openDoors();
-			return;
-		}
-		if (targetFloor > floor ) elevator.up() ;
-		else elevator.down();
+		// TODO
 	}
 
 	@Override
@@ -97,23 +59,17 @@ public class ElevatorSoftware implements ElevatorListener, ElevatorUserInterface
 	
 	@Override
 	public void stop() {
-		started = !started;
-		if ( started ) elevator.start();
-		else elevator.stop();
+		// TODO
 	}
 	
 	@Override
 	public void keepOpen() {
-		if ( mayKeepOpen ) {
-			elevator.openDoors();
-			return;
-		}
+		// TODO
 	}
 	
 	@Override
 	public void alarm() {
-		// TODO Auto-generated method stub
-		
+		// TODO
 	}
 	
 
